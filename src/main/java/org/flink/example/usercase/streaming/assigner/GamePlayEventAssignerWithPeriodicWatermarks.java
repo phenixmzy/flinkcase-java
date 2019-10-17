f277b4b63652a4f81053fe082bb774ce4940885d
phenixmzy <wingmazhiyong@gmail.com>
2019-10-17 22:25:52 +0800

    Add HDFS Audit Operation Window Application

--- src/main/java/org/flink/example/usercase/streaming/assigner/GamePlayEventAssignerWithPeriodicWatermarks.java
+++ src/main/java/org/flink/example/usercase/streaming/assigner/GamePlayEventAssignerWithPeriodicWatermarks.java
@@ -7,22 +7,41 @@ import org.flink.example.usercase.model.GamePlayEvent;
 import javax.annotation.Nullable;
 
 public class GamePlayEventAssignerWithPeriodicWatermarks implements AssignerWithPeriodicWatermarks<GamePlayEvent> {
-    protected long currentTimestamp = Long.MIN_VALUE;
+    // 当前最大时间
+    protected long currentMaxTimestamp = Long.MIN_VALUE;
+
+    // 延时设定,表示在maxOutOfOrderness秒内有效,超过maxOutOfOrderness秒的数据被认定为迟到的事件
     protected long maxOutOfOrderness = 0L;
 
     public GamePlayEventAssignerWithPeriodicWatermarks(final long maxOutOfOrderness) {
         this.maxOutOfOrderness = maxOutOfOrderness;
     }
 
+    /**
+     * 定义水位线的生成
+     * @return
+     */
     @Nullable
     @Override
     public Watermark getCurrentWatermark() {
-        return new Watermark(currentTimestamp == Long.MIN_VALUE ? Long.MIN_VALUE : currentTimestamp - maxOutOfOrderness);
+        // 根据最大事件时间 - 最大乱序时延长度,最后得到水位线对象Watermark
+        return new Watermark(currentMaxTimestamp == Long.MIN_VALUE ? Long.MIN_VALUE : currentMaxTimestamp - maxOutOfOrderness);
     }
 
+    /**
+     * 定义抽取TimeStamp的逻辑
+     * @param gamePlayEvent
+     * @param previousEventTimeStamp
+     * @return
+     */
     @Override
-    public long extractTimestamp(GamePlayEvent gamePlayEvent, long timeStamp) {
-        currentTimestamp = Math.max(gamePlayEvent.getLeaveTime(), currentTimestamp);
-        return currentTimestamp;
+    public long extractTimestamp(GamePlayEvent gamePlayEvent, long previousEventTimeStamp) {
+        // 获取当前事件时间
+        int currentTimeStamp = gamePlayEvent.getLeaveTime();
+        // 对比当前事件时间和历史最大时间,将最新的时间赋值给currentMaxTimestamp
+        currentMaxTimestamp = Math.max(currentTimeStamp, currentMaxTimestamp);
+
+        // 返回当前事件事件
+        return currentTimeStamp;
     }
 }
