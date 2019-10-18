e2820bb36d21c2922ca4e7e91bf8f06612eec6e8
mazhiyong <wingmazhiyong@gmail.com>
2019-10-18 18:27:56 +0800

    update for window application[GamePlayWindowApplication has OK]

--- src/main/java/org/flink/example/usercase/streaming/application/audit/HDFSAuditOperationWindowApplication.java
+++ src/main/java/org/flink/example/usercase/streaming/application/audit/HDFSAuditOperationWindowApplication.java
@@ -1,10 +1,14 @@
 package org.flink.example.usercase.streaming.application.audit;
 
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 import org.flink.example.common.constant.PropertiesConstants;
@@ -21,15 +25,24 @@ public class HDFSAuditOperationWindowApplication {
         ParameterTool parameterTool = ExecutionEnvUtil.createParameterTool(args);
         StreamExecutionEnvironment env = ExecutionEnvUtil.prepare(parameterTool);
         DataStreamSource<String> source = KafkaConfigUtil.buildSource(env);
-        source.map(hdfsAuditJson ->{
-            HDFSAuditEvent event = GsonUtil.fromJson(hdfsAuditJson, HDFSAuditEvent.class);
-            return event;
-        }).assignTimestampsAndWatermarks(new HDFSAuditEventAssignerWithPeriodicWatermarks(parameterTool.getLong("window.max.outoforderness")))
-        .keyBy(0).map(item -> Tuple2.of(item.getCmd(), 1))
+        source.map(new MapFunction<String, Tuple3<String, Long, Integer>>() {
+
+            @Override
+            public Tuple3<String, Long, Integer> map(String s) throws Exception {
+                HDFSAuditEvent event = GsonUtil.fromJson(s, HDFSAuditEvent.class);
+                return Tuple3.of(event.getCmd(), event.getTimeStampMS(),1);
+            }
+    }).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<Tuple3<String,Long,Integer>>(Time.seconds(parameterTool.getLong(PropertiesConstants.FLINK_WINDOW_MAX_OUTOFORDERNESS))) {
+            @Override
+            public long extractTimestamp(Tuple3<String,Long,Integer> event) {
+                return event.f1;
+            }
+        })
+        .keyBy(0)
         .timeWindowAll(Time.of(parameterTool.getLong(PropertiesConstants.FLINK_WINDOW_SIZE),SECONDS),
                 Time.of(parameterTool.getLong(PropertiesConstants.FLINK_WINDOW_SLIDE),SECONDS))
-                .sum(0)
-                .addSink(new FlinkKafkaProducer011(parameterTool.getRequired("kafka.brokers"),
+                .sum(0).map(item -> item.f0 + " " + item.f2)
+                .addSink(new FlinkKafkaProducer011(parameterTool.getRequired(PropertiesConstants.KAFKA_BROKERS_KEY),
                         parameterTool.getRequired(PropertiesConstants.KAFKA_SINK_TOPIC_KEY),
                         new SimpleStringSchema()));
 
