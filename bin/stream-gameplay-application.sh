931473c1bf41dc3e335a999a45237346d84b2601
phenixmzy <wingmazhiyong@gmail.com>
2020-08-31 20:07:06 +0800

    update

--- bin/stream-gameplay-application.sh
+++ bin/stream-gameplay-application.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 /usr/local/flink/bin/flink run \
-	-m yarn-cluster \
-	-ys 3 \
-	-yjm 2048m \
-	-ytm 3096m \
-	-yn 4 \
-	-d \
-	-c org.flink.example.usercase.streaming.application.gameplay.GamePlayStreamingApplication \
-	/root/flinkcase-java/flinkcase-java-0.0.1.jar \
-	--kafka.source.topic gameplay-log-input \
-	--kafka.sink.topic gameplay  \
-	--bootstrap.servers yzj-client-01:9092,yzj-client-02:9092,yzj-client-03:9092 \
-	--group.id test-java-gameplay-log-input-gid \
-	--zookeeper.connect yzj-client-01:2181,yzj-client-02:2181,yzj-client-03:2181/kafka \
-	--task-num 3
+        -m yarn-cluster \
+        -ynm flink-gameplay-application \
+        -yqu root.statis \
+        -d \
+        -c org.flink.example.usercase.streaming.application.gameplay.GamePlayStreamingApplication \
+        /home/mark/flinkcase-java/jar/flinkcase-java-0.0.1.jar \
+        --kafka.source.topic source-gameplay \
+        --kafka.sink.topic test  \
+        --bootstrap.servers vmphenix:9092,vmnarsi:9092,vmramon:9092 \
+        --group.id test-source-gameplay-gid \
+        --zookeeper.connect vmphenix:2181,vmnarsi:2181,vmramon:2181
+
