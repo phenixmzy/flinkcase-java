7c98e505a74b62712133a5d0f36c8b1ff4a7392c
phenixmzy <wingmazhiyong@gmail.com>
2021-06-16 17:18:26 +0800

    update game-play shell

--- bin/make-gameplayevnet-application.sh
+++ bin/make-gameplayevnet-application.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
-/usr/local/flink/bin/flink run \
-	-m yarn-cluster \
-	-ys 3 \
-	-yjm 2048m \
-	-ytm 3096m \
-	-yn 4 \
-	-d \
-	-c org.flink.example.usercase.streaming.application.gameplay.MakeGamePlayEventApplication \
-	/root/flinkcase-java/flinkcase-java-0.0.1.jar \
-	--gameplay.record.max.num 10000 \
-	--gameplay.gid.max.num 100000  \
-	--gameplay.uid.max.num 10000000 \
-	--gameplay.delay.max.num 5\
-	--gameplay.timelen.max.num 300\
-	--kafka.sink.brokers yzj-client-01:9092,yzj-client-02:9092,yzj-client-03:9092 \
-	--kafka.sink.topic gameplay \
-	--group.id make-gameplayenvent-gid \
-	--task-num 3
+export HADOOP_CONF_DIR=/etc/hadoop/conf
+export HBASE_CONF_DIR=/etc/hbase/conf
+export HADOOP_HOME=/opt/cloudera/parcels/CDH
+export HADOOP_CLASSPATH=/opt/cloudera/parcels/CDH/jars/*
+export FLINK_HOME=/opt/cloudera/parcels/FLINK/lib/flink
+
+/usr/local/flink/bin/flink  run -t yarn-per-job --detached \
+        -Dflink.yarn.queue=root.default -Dflink.tm.slot=2 \
+        -Dsecurity.kerberos.login.contexts=Client,KafkaClient \
+        -Dsecurity.kerberos.login.keytab=/var/data/service/keytabs/services.keytab \
+        -Dsecurity.kerberos.login.principal=flink@JDY.KD.INTERNAL \
+        -Djava.security.krb5.conf=/etc/krb5.conf \
+        -Djava.security.auth.login.config=/var/data/service/keytabs/flink_jaas.conf \
+        -c org.flink.example.usercase.streaming.application.gameplay.MakeGamePlayEventApplication \
+        /home/kduser/flinkcase-java/flinkcase-java-0.0.1.jar \
+        --gameplay.record.max.num 10000 \
+        --gameplay.gid.max.num 100000  \
+        --gameplay.uid.max.num 10000000 \
+        --gameplay.delay.max.num 5 \
+        --gameplay.timelen.max.num 300\
+	    --kafka.sink.brokers yzj-client-01:9092,yzj-client-02:9092,yzj-client-03:9092 \
+	    --kafka.sink.topic gameplay \
+        --group.id make-gameplayenvent-gid \
+        --task-num 2
