401106fd54cf43d915eae0c794fcae3bfa2aebfd
phenixmzy <wingmazhiyong@gmail.com>
2021-06-17 15:49:53 +0800

    fix gameplay.timelen.max.num 参数

--- bin/make-gameplayevnet-application.sh
+++ bin/make-gameplayevnet-application.sh
@@ -18,7 +18,7 @@ export FLINK_HOME=/opt/cloudera/parcels/FLINK/lib/flink
         --gameplay.gid.max.num 100000  \
         --gameplay.uid.max.num 10000000 \
         --gameplay.delay.max.num 5 \
-        --gameplay.timelen.max.num 300\
+        --gameplay.timelen.max.num 300 \
 	    --kafka.sink.brokers yzj-client-01:9092,yzj-client-02:9092,yzj-client-03:9092 \
 	    --kafka.sink.topic gameplay \
         --group.id make-gameplayenvent-gid \
